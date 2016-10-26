4ad422c5dd696ed211efa077dd6e64ab973dc76e
lyonya <leonbelyak@mail.ru>
2016-10-26 13:22:12 +0300

    Исправление условий

--- src/tbitfield.cpp
+++ src/tbitfield.cpp
@@ -53,40 +53,23 @@ int TBitField::GetLength(void) const // получить длину (к-во б�
 
 void TBitField::SetBit(const int n) // установить бит
 {
-	if (n < 0)
-	{
+	if ((n < 0) || (n>BitLen))
 		throw "WARNING!";
-	}
-	if (n > BitLen)
-	{
-		throw "WARNING!";
-	}
+
 	pMem[GetMemIndex(n)] |= GetMemMask(n);
 }
 
 void TBitField::ClrBit(const int n) // очистить бит
 {
-	if (n < 0)
-	{
+	if ((n < 0) || (n>BitLen))
 		throw "WARNING!";
-	}
-	if (n > BitLen)
-	{
-		throw "WARNING!";
-	}
 	pMem[GetMemIndex(n)] &= ~GetMemMask(n);
 }
 
 int TBitField::GetBit(const int n) const // получить значение бита
 {
-	if (n < 0)
-	{
+	if ((n < 0) || (n>BitLen))
 		throw "WARNING!";
-	}
-	if (n > BitLen)
-	{
-		throw "WARNING!";
-	}
 	return ((pMem[GetMemIndex(n)] & GetMemMask(n)) != 0);
 }
 
